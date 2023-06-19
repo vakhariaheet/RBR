c56b0dc5781c5dbc647bf5db55f38f0873d9dec5
Heet Vakharia <heetkv@gmail.com>
2023-06-20 01:53:17 +0530

    Updated Course

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,15 +17,27 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-    <html lang='en'>
-      <head>
-        
-      </head>
+		<html lang='en'>
+			<head>
+				<Script
+					id='clarity'
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: `
+					
+					(function(c,l,a,r,i,t,y){
+						c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+						t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+						y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+					})(window, document, "clarity", "script", "hmhnomnyax");
+				`,
+					}}
+				/>
+			</head>
 			<body className={inter.className}>
 				{children}
-				<Script src="https://documentcloud.adobe.com/view-sdk/main.js"></Script>
+				<Script src='https://documentcloud.adobe.com/view-sdk/main.js'></Script>
 			</body>
 		</html>
-
 	);
 }
