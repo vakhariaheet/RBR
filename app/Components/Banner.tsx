5d58b3228f63bf8e244742682e7063d202b380e0
Heet Vakharia <heetkv@gmail.com>
2023-08-17 00:50:16 +0530

    Added Adsterra

---
+++ app/Components/Banner.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef } from 'react';
+export default function Banner(): JSX.Element {
+	const banner = useRef<HTMLDivElement>(null);
+
+	const atOptions = {
+		key: 'a9975221053c7830bbe80f0c27cd5d4a',
+		format: 'iframe',
+		height: 50,
+		width: 320,
+		params: {},
+	};
+	useEffect(() => {
+		if (banner.current && !banner.current.firstChild) {
+			const conf = document.createElement('script');
+			const script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src =
+				location.protocol +
+				`//www.profitablecreativeformat.com/${atOptions.key}/invoke.js`;
+			conf.innerHTML = `atOptions = ${JSON.stringify(atOptions)}`;
+			banner.current.append(conf);
+			banner.current.append(script);
+		}
+	}, [banner]);
+
+	return (
+		<div
+			className='mx-2 my-5 border border-gray-200 justify-center items-center text-white text-center'
+			ref={banner}
+		></div>
+	);
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
+import Banner from './Components/Banner';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -15,16 +16,20 @@ export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
-}) {
+	}) {
+
 	return (
 		<html lang='en'>
 			<head>
 				
 			</head>
 			<body className={inter.className}>
+				<Banner/>
 				{children}
 				<Script src='https://documentcloud.adobe.com/view-sdk/main.js'></Script>
+				
 			</body>
 		</html>
 	);
 }
+
